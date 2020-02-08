212ca7aba6b6e50089398e722d259dfb8c794cb2
Irène <irene.nastatchenko@gmail.com>
2020-02-08 13:23:19 +0200

    added w2fex3_guess_my_number.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 23:26:54 2020
+
+@author: irina
+"""
+
+print("Hello, a great Data Scientist!")
+print('I like 6.00.1x!')
